19ebe3dd07316f38cc8a04222e9f34b4cf3b3e2e
zahra-ove <50420986+zahra-ove@users.noreply.github.com>
2022-05-20 16:19:00 +0430

    Update pansion_mod10_changes.sql
    alter table statusmalichangedorders

--- pansion_mod10_changes.sql
+++ pansion_mod10_changes.sql
@@ -27,3 +27,38 @@ CREATE TABLE `usertotalstatusmalitransactions`  (
 );
 
 //=================================================================================
+drop table statusmalichangedorders;
+CREATE TABLE `statusmalichangedorders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `order_id` int(11) NOT NULL,
+  `returntocustom_tranaction_id` int null default null,
+  `returntowallet_tranaction_id` int null default null,
+  `jarime_tranaction_id` int null default null,
+	
+  `status_order_id` int(11) NOT NULL,
+  `order_finally_price` decimal(15,2) DEFAULT NULL,
+  `total_paid_amount` decimal(15,2) DEFAULT NULL,
+  `returned_to_customer_amount` decimal(15,2) DEFAULT NULL,
+  `returned_to_wallet_amount` decimal(15,2) DEFAULT NULL,
+  `returned_type` enum('naqd','wallet','naqd_wallet') DEFAULT NULL,
+  `has_jarime` enum('0','1') DEFAULT NULL,
+  `jarime_amount` decimal(15,2) DEFAULT NULL,
+  `jarime_is_paid` enum('0','1') DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+
+  KEY `statusmalichangedorders_orders_indx` (`order_id`) USING BTREE,
+  KEY `statusmalichangedorders__status_orders_indx` (`status_order_id`) USING BTREE,
+	
+	KEY statusmalichangedorders_transaction_indx1 (returntocustom_tranaction_id),
+	KEY statusmalichangedorders_transaction_indx2 (returntowallet_tranaction_id),
+	KEY statusmalichangedorders_transaction_indx3 (jarime_tranaction_id),
+	
+  CONSTRAINT `statusmalichangedorders__status_orders_fk` FOREIGN KEY (`status_order_id`) REFERENCES `status_orders` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `statusmalichangedorders_orders_fk` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON UPDATE CASCADE,
+	
+	CONSTRAINT statusmalichangedorders_transaction_fk1 FOREIGN KEY  (returntocustom_tranaction_id) REFERENCES transactions (id)  ON DELETE RESTRICT ON UPDATE CASCADE,
+	CONSTRAINT statusmalichangedorders_transaction_fk2 FOREIGN KEY  (returntowallet_tranaction_id) REFERENCES transactions (id)  ON DELETE RESTRICT ON UPDATE CASCADE,
+	CONSTRAINT statusmalichangedorders_transaction_fk3 FOREIGN KEY  (jarime_tranaction_id) REFERENCES transactions (id)  ON DELETE RESTRICT ON UPDATE CASCADE
+);
